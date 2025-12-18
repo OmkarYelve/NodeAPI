6a27307d55dfbe7fee3211288a8af4b13f39df6b
Omkar Yelve <omkaryelve1@gmail.com>
2025-12-18 17:08:57 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,38 @@
+import http from "node:http";
+import { getDataFromDB } from "./database/db.js";
+import { sendJSONResponse } from "./utilities/sendJSONResponse.js";
+import { getDataByPathParams } from "./utilities/getDataByPathParams.js";
+import { getDataByQueryParams } from "./utilities/getDataByQueryParams.js";
+const PORT = 8000;
+
+const server = http.createServer(async (req, res) => {
+  const destinations = await getDataFromDB();
+
+  const urlObj = new URL(req.url, `http://${req.headers.host}`);
+  const queryObj = Object.fromEntries(urlObj.searchParams);
+
+  if (urlObj.pathname === "/api" && req.method === "GET") {
+    let filteredData = getDataByQueryParams(destinations, queryObj);
+
+    sendJSONResponse(res, 200, filteredData);
+  } else if (req.url.startsWith("/api/continent") && req.method === "GET") {
+    const continent = req.url.split("/").pop();
+    const filteredData = getDataByPathParams(
+      destinations,
+      "continent",
+      continent
+    );
+    sendJSONResponse(res, 200, filteredData);
+  } else if (req.url.startsWith("/api/country") && req.method === "GET") {
+    const country = req.url.split("/").pop();
+    const filteredData = getDataByPathParams(destinations, "country", country);
+    sendJSONResponse(res, 200, filteredData);
+  } else {
+    sendJSONResponse(res, 404, {
+      error: "not found",
+      message: "The requested route does not exist",
+    });
+  }
+});
+
+server.listen(PORT, () => console.log(`server connected on port: ${PORT}`));
